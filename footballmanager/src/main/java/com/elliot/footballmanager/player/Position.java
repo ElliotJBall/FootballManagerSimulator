00f1c5d1fd56e0c15f5303ed7708b495a5db399e
ElliotJBall <ElliotJordBall@gmail.com>
2018-06-17 19:43:04 +0100

    Fixed broken method call to incorrectly spelt method

--- footballmanager/src/main/java/com/elliot/footballmanager/player/Position.java
+++ footballmanager/src/main/java/com/elliot/footballmanager/player/Position.java
@@ -50,7 +50,7 @@ public enum Position {
 	}
 
 	private static void initializeGeneralisedPositions() {
-		initalizeGeneralisedDefenderPositions();
+		initializeGeneralisedDefenderPositions();
 		initializeGeneralisedMidfielderPositions();
 		initializeGeneralisedStrikerPositions();
 	}
